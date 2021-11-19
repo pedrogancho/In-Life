06318ace4b1bd9822538b62132b88275fb5e3bd7
Luisfdearaujo <90002192+Luisfdearaujo@users.noreply.github.com>
2021-11-19 14:14:41 +0000

    Add the routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
 const Admin = require("../models/admin-model");
 const Ambassador = require("../models/ambassador-model");
+
 const Client = require("../models/client-model");
+
+module.exports = router;
