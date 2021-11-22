9bc2cb8500ca9be9deb5ac758e140dbcffbcfcd0
Luisfdearaujo <90002192+Luisfdearaujo@users.noreply.github.com>
2021-11-22 16:45:34 +0000

    Create Clients DB and Views

--- routes/client.js
+++ routes/client.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const Client = require("../models/client-model");
 
 //Get Client page => Show all Clients
 router.get("/clients", (req, res) => {
-  res.render("client-view");
+  Client.find()
+  .then((clientsFromDb)=>{
+
+    res.render("client-view", {clientsFromDb});
+  });
 });
 
 //add clients
@@ -10,7 +15,17 @@ router.get("/clientsadd", (req, res) => {
   res.render("client-add");
 });
 
+router.post("/clientsadd", (req, res) => {
+  const { name, lastName, email, promocode } = req.body;
 
-
+  Client.create({
+    name: name,
+    lastName: lastName,
+    email: email,
+    promocode: promocode,
+  })
+    .then((createdClient) => res.redirect(`/clientsadd`))
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
